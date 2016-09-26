6aef04aff7dd9431ba14d426d30977bcee2609e6
dantleech <dan.t.leech@gmail.com>
2016-09-26 14:32:27 +0100

    Added filter registry

---
+++ tests/Unit/FilterRegistryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Psi\Component\ResourceBrowser\Tests\Unit;
+
+use Psi\Component\ResourceBrowser\FilterInterface;
+use Psi\Component\ResourceBrowser\FilterRegistry;
+
+class FilterRegistryTest extends \PHPUnit_Framework_TestCase
+{
+    private $registry;
+
+    public function setUp()
+    {
+        $this->filter1 = $this->prophesize(FilterInterface::class);
+        $this->registry = new FilterRegistry([
+            'one' => $this->filter1->reveal()
+        ]);
+    }
+
+    /**
+     * It should return the named filter.
+     */
+    public function testReturnNamedFilter()
+    {
+        $filter = $this->registry->get('one');
+        $this->assertSame($this->filter1->reveal(), $filter);
+    }
+
+    /**
+     * It should throw an exception if the filter is not known.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Unknown filter "five". Known filters: "one"
+     */
+    public function testThrowExceptionNotFound()
+    {
+        $this->registry->get('five');
+    }
+
+}
